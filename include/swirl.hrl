2749950396878076abd3439796013240c9fb405c
Louis-Philippe Gauthier <lpgauth@gmail.com>
2014-11-21 14:23:39 -0500

    Switch back to proplists

--- include/swirl.hrl
+++ include/swirl.hrl
@@ -16,8 +16,8 @@
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 -define(L(Key, List), swirl_utils:lookup(Key, List)).
 -define(L(Key, List, Default), swirl_utils:lookup(Key, List, Default)).
--define(LM(Key, Map), ?LM(Key, Map, undefined)).
--define(LM(Key, Map, Default), maps:get(Key, Map, Default)).
+-define(LM(Key, List), swirl_utils:lookup(Key, List)).
+-define(LM(Key, List, Default), swirl_utils:lookup(Key, List, Default)).
 -define(NULL, undefined).
 
 %% records
